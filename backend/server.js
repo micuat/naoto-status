0ba4e3f1a3e816af9f0fa58c3e47e13f49e51df5
micuat <micuat@gmail.com>
2023-11-08 20:51:18 +0100

    path

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 import { AceBase } from "acebase";
 const options = { logLevel: 'warn', storage: { path: '.' } }; // optional settings
-const db = await new AceBase('/data/mydb', options);  // Creates or opens a database with name "mydb"
+const db = await new AceBase('mydb', options);  // Creates or opens a database with name "mydb"
 
 app.use(express.json());
 
